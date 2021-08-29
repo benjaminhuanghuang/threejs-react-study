44104ea61ca40f1f1c601a6b4243cbb75cae38b2
Benjamin Huang <benjaminhuanghuang@gmail.com>
2021-08-28 22:44:03 -0700

    cube

--- threejs-react-app/src/views/start/index.js
+++ threejs-react-app/src/views/start/index.js
@@ -1,10 +1,59 @@
-import './index.scss'
+import { useEffect, useRef, useCallback } from "react";
 
-const View = () =>{
-    return (
-        <div className="page"></div>
-    )
-}
+import * as THREE from "three";
+
+import "./index.scss";
+
+const Page = () => {
+  const Body = useRef();
+  // Scene = new THREE.Scene()
+  const Scene = useRef(new THREE.Scene()).current;
+  const Camera = useRef(new THREE.PerspectiveCamera()).current;
+  const Render = useRef(new THREE.WebGLRenderer({ antialias: true })).current;
+  const Meshs = useRef([]).current;
+
+  const createRect = useCallback(() => {
+    const rect = new THREE.BoxBufferGeometry(2, 2, 2)
+    const meshBasicMater = new THREE.MeshBasicMaterial({ color: 'red' })
+    const mesh = new THREE.Mesh(rect, meshBasicMater)
+    mesh.position.set(0, 0, 0)
+    Scene.add(mesh)
+    Meshs.push(mesh)
+}, [])
 
 
-export default View;
+  const init = useCallback(() => {
+    Render.setSize(Body.current.offsetWidth, Body.current.offsetHeight)
+    // 设置相机参数
+    Camera.aspect = Body.current.offsetWidth / Body.current.offsetHeight
+    Camera.fov = 45
+    Camera.near = 1
+    Camera.far = 1000
+    Camera.position.set(0, 10, 20)
+    Camera.lookAt(0, 0, 0)
+    Camera.updateProjectionMatrix()
+  }, [Render, Body]);
+
+
+  const renderScene = useCallback(() => {
+    Render.render(Scene, Camera)
+    Meshs.forEach(item => {
+        item.rotation.x += 0.5 / 180 * Math.PI 
+        item.rotation.y += 0.5 / 180 * Math.PI 
+    })
+    window.requestAnimationFrame(() => renderScene())
+}, [Render, Meshs])
+
+
+
+  useEffect(() => {
+    Body.current.append(Render.domElement)
+        init()
+        createRect()
+        renderScene()
+  }, []);
+
+  return <div className="page" ref={Body}></div>;
+};
+
+export default Page;
